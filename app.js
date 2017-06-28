04630ed29e62edb21c7fa944858110336b5ef175
Gisle K <gislekvalvik@gmail.com>
2017-06-28 12:51:28 +0000

    Added bootstrap and campground grid

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.set("view engine", "ejs");
 var campgrounds = [
             {name: "Salmon Creek", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
             {name: "Granite Hill", image: "https://farm9.staticflickr.com/8038/7930463550_42c3f82870.jpg"},
+            {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
+            {name: "Salmon Creek", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
+            {name: "Granite Hill", image: "https://farm9.staticflickr.com/8038/7930463550_42c3f82870.jpg"},
             {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}
         ];
 
