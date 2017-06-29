f8d203807de7625949dc335f014485a983cbc87e
Gisle K <gislekvalvik@gmail.com>
2017-06-29 12:35:30 +0000

    Added show page

--- app.js
+++ app.js
@@ -10,65 +10,65 @@ app.set("view engine", "ejs");
 // SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
     name: String,
-    image: String
+    image: String,
+    description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
-
-// Campground.create({
-//     name: "Granite Hill",
-//     image: "https://farm9.staticflickr.com/8038/7930463550_42c3f82870.jpg"
-// }, function(err, campground){
-//     if(err){
-//         console.log("ERROR");
-//         console.log(err);
-//     } else {
-//         console.log("NEWLY CREATED CAMPGROUND");
-//         console.log(campground);
-//     }
-// });
-
-// var campgrounds = [
-//             {name: "Salmon Creek", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
-//             {name: "Granite Hill", image: "https://farm9.staticflickr.com/8038/7930463550_42c3f82870.jpg"},
-//             {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"},
-//             {name: "Salmon Creek", image: "https://farm1.staticflickr.com/22/31733208_3190a1e982.jpg"},
-//             {name: "Granite Hill", image: "https://farm9.staticflickr.com/8038/7930463550_42c3f82870.jpg"},
-//             {name: "Mountain Goat's Rest", image: "https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg"}
-//         ];
-
 
 app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// INDEX - show all campgrounds
 app.get("/campgrounds", function(req, res) {
     Campground.find({}, function(err, campgrounds){
         if(err) {
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds: campgrounds});        
+            res.render("index", {campgrounds: campgrounds});        
         }
     });
 });
 
+// CREATE - add new campground to DB
 app.post("/campgrounds", function(req, res) {
-   //get data from form and add to campgrounds array
+   //get data from form and add to a new campgrounds object
    var name = req.body.name;
    var image = req.body.image;
-   var newCampground = {name: name, image: image};
+   var desc = req.body.description;
+   var newCampground = {name: name, image: image, description: desc};
    
+   // Create a new campground and save to DB
    Campground.create(newCampground, function(err, campground){
        if(err){
             console.log(err);
        } else {
+           // Redirect back to campgrounds page
             res.redirect("/campgrounds");        
        }
    });
 });
 
+// NEW - show form to create new campground
 app.get("/campgrounds/new", function(req, res) {
-   res.render("new.ejs"); 
+   
+   res.render("new"); 
+});
+
+// SHOW - Show info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+    // Find campground with the provided ID
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err) {
+            console.log(err);
+        } else {
+            // Render show tempalte with that campground
+            res.render("show", {campground: foundCampground});        
+        }
+    });
+    
+    
 });
 
 
